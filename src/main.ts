283b78c3b0f8ef24db9eb0993414a2ec140e8092
Ahmet Melih Kızmaz <melihkizmaz@icloud.com>
2022-08-17 20:01:36 +0300

    fix heroku bug

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,21 @@
 import { ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
+import { Transport } from '@nestjs/microservices';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
   const port = configService.get('port');
+  app.connectMicroservice({
+    transport: Transport.TCP,
+    options: {
+      port: '0.0.0.0',
+    },
+  });
   app.useGlobalPipes(new ValidationPipe());
+  await app.startAllMicroservices();
   await app.listen(port);
 }
 bootstrap();
